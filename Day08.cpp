e43c771d16aa3066221561632767bafed3a77814
adarsh-patnaik <adarsh.patnaik@outlook.com>
2026-06-06 21:07:27 +0530

    Day 08

---
+++ Day08.cpp
@@ -0,0 +1,46 @@
+class MinStack {
+public:
+    stack<long long> st;
+    long long min;
+    MinStack() { min = LLONG_MAX; }
+
+    void push(int val) {
+        long long x = (long long)val;
+        if (st.size() == 0) {
+            st.push(x);
+            min = x;
+        } else if (x >= min)
+            st.push(x);
+        else {
+            st.push(2 * x - min);
+            min = x;
+        }
+    }
+
+    void pop() {
+        if (st.top() < min) {
+            long long oldMin = 2 * min - st.top();
+            min = oldMin;
+        }
+        st.pop();
+    }
+
+    int top() {
+        if (st.top() < min)
+            return min;
+        else {
+            return st.top();
+        }
+    }
+
+    int getMin() { return min; }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
