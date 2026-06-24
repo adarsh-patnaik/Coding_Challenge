01d883e1246f1ef2da46dce5b5a37baaee7e2ee4
adarsh-patnaik <adarsh.patnaik@outlook.com>
2026-06-24 11:54:35 +0530

    Day 26

---
+++ Day26.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+void solve()
+{
+    string s;
+    cin >> s;
+
+    if(s.find("1111111") != string::npos || 
+        s.find("0000000") != string::npos){
+            cout << "YES\n";
+        }
+    else{
+        cout << "NO\n";
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll test = 1;
+    // cin>>test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
